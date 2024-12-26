903319b3fd2616317073310b988513b12dede282
Jason Yan <yww325@gmail.com>
2024-12-26 15:55:55 +0800

    Update shadowsocks-all.sh
    remove python libs from apt depends due to python 3 installed.

--- shadowsocks-all.sh
+++ shadowsocks-all.sh
@@ -566,7 +566,7 @@ install_dependencies(){
         done
     elif check_sys packageManager apt; then
         apt_depends=(
-            gettext build-essential unzip gzip python python-dev python-setuptools curl openssl libssl-dev
+            gettext build-essential unzip gzip curl openssl libssl-dev
             autoconf automake libtool gcc make perl cpio libpcre3 libpcre3-dev zlib1g-dev libev-dev libc-ares-dev git qrencode
         )
 
